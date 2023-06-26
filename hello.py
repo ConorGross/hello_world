a86d1c02e4063fbece983cbbe03e68477e793bd0
ConorGross <137304309+ConorGross@users.noreply.github.com>
2023-06-26 15:07:24 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello World!")
 Print("\t This is my first Git repository on Github.")
+
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+
+name = "Julia"
+print_message(name)
